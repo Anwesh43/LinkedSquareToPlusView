508fed15550c2f9f63adfabae315b30a2c91f0a4
Anwesh43 <anweshthecool0@gmail.com>
2019-04-15 10:11:45 +0530

    adding Int and Float extension methods

--- squaretoplusview/src/main/java/com/anwesh/uiprojects/squaretoplusview/SquareToPlusView.kt
+++ squaretoplusview/src/main/java/com/anwesh/uiprojects/squaretoplusview/SquareToPlusView.kt
@@ -22,3 +22,14 @@ val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val angleDeg : Float = 90f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirrorValue(a : Int, b : Int) : Float {
+    val k : Float = scaleFactor()
+    return (1 - k) * a.inverse() + k * b.inverse()
+}
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * scGap * dir
+fun Float.sf() : Float = 1f - 2 * this
