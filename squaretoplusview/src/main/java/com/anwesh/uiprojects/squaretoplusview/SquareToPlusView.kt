d96f6acf616aac99022cac49d08a03fa7d372f05
Anwesh43 <anweshthecool0@gmail.com>
2019-04-15 10:37:28 +0530

    using renderer in view

--- squaretoplusview/src/main/java/com/anwesh/uiprojects/squaretoplusview/SquareToPlusView.kt
+++ squaretoplusview/src/main/java/com/anwesh/uiprojects/squaretoplusview/SquareToPlusView.kt
@@ -70,14 +70,17 @@ fun Canvas.drawSTPNode(i : Int, scale : Float, paint : Paint) {
 
 class SquareToPlusView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
