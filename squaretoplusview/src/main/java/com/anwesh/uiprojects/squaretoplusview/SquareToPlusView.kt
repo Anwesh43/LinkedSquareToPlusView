8cb5435a03124f232bee415d2618e14a60383623
Anwesh43 <anweshthecool0@gmail.com>
2019-04-15 10:08:31 +0530

    adding libraries and constants

--- squaretoplusview/src/main/java/com/anwesh/uiprojects/squaretoplusview/SquareToPlusView.kt
+++ squaretoplusview/src/main/java/com/anwesh/uiprojects/squaretoplusview/SquareToPlusView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.squaretoplusview
 /**
  * Created by anweshmishra on 15/04/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val parts : Int = 2
+val scGap : Float = 0.5f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val angleDeg : Float = 90f
